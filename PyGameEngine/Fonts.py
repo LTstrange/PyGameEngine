34453cd44c643c0df54fb0ea53fa161677811788
LTstrange <835422774@qq.com>
2021-10-15 12:08:06 +0800

    archive class Font

--- PyGameEngine/Fonts.py
+++ PyGameEngine/Fonts.py
@@ -4,16 +4,23 @@
 
 
 import pygame
+from pygame import font
 
 
 class Fonts:
     def __init__(self):
         self.default_font = pygame.font.SysFont(pygame.font.get_default_font(), 10)
-        self.fonts = []
+        self.fonts = dict()
+
 
     def add_font(self, name, size):
-        new_font = pygame.font.Font(name, size)
-        self.fonts[(name, size)] = new_font
+        new_font = pygame.font.SysFont(name, size)
+        if name in self.fonts.keys():
+            font = self.fonts[name]
+            self.fonts[name][size] = new_font
+        else:
+            self.fonts[name] = {size:new_font}
+
 
     def get_font_by_name(self, name=None):
         if name == None:
@@ -21,3 +28,15 @@ class Fonts:
         else:
             print(self.fonts.keys)
 
+
+if __name__ == "__main__":
+    pygame.init()
+    fonts = Fonts()
+    name = pygame.font.get_default_font()
+    print(pygame.font.get_fonts())
+    fonts.add_font(name, 20)
+    print(fonts.fonts)
+    fonts.add_font(name, 30)
+    print(fonts.fonts)
+    fonts.add_font('piboto', 20)
+    print(fonts.fonts)
