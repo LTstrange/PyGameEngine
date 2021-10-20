e88a2179ab4c3d54a21b9d1746ae4d61dc573702
LTstrange <835422774@qq.com>
2021-10-20 18:23:21 +0800

    create TileMap.py, implement a little.

--- PyGameEngine/Input.py
+++ PyGameEngine/Input.py
@@ -2,6 +2,7 @@
 # @Time    : 2021/10/12 10:31
 # @Author  : LTstrange
 import pygame
+from pygame.math import Vector2
 from pygame.sprite import Sprite, AbstractGroup
 
 
@@ -9,9 +10,11 @@ class Input:
     def __init__(self, game):
         self.game = game
         self.mouse = Mouse()
-        self.axis = pygame.math.Vector2()
+        self.axis = Vector2()
+        self.hit = Vector2()
 
     def update(self):
+        self.hit = Vector2()
         self.mouse.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -26,12 +29,16 @@ class Input:
             if event.type == pygame.KEYDOWN:
                 if event.key in [pygame.K_w, pygame.K_UP]:
                     self.axis.y -= 1
+                    self.hit.y -= 1
                 if event.key in [pygame.K_s, pygame.K_DOWN]:
                     self.axis.y += 1
+                    self.hit.y += 1
                 if event.key in [pygame.K_a, pygame.K_LEFT]:
                     self.axis.x -= 1
+                    self.hit.x -= 1
                 if event.key in [pygame.K_d, pygame.K_RIGHT]:
                     self.axis.x += 1
+                    self.hit.x = 1
             if event.type == pygame.KEYUP:
                 if event.key in [pygame.K_w, pygame.K_UP]:
                     self.axis.y -= -1
--- PyGameEngine/Object.py
+++ PyGameEngine/Object.py
@@ -2,16 +2,15 @@
 # @Time    : 2021/10/12 10:59
 # @Author  : LTstrange
 import pygame
-from pygame.color import THECOLORS
 from pygame.math import Vector2
 from pygame.sprite import Sprite, AbstractGroup
 
 
 class Object(Sprite):
-    def __init__(self, pos, size, color, *groups: AbstractGroup):
+    def __init__(self, game, pos, size, color, *groups: AbstractGroup):
         super().__init__(*groups)
+        self.game = game
         self.pos = Vector2(pos)
-        self.size = size
 
         self._image = pygame.Surface(size)
         self._image.fill(color)
